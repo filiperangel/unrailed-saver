11ab1fc8349e74c746170382ed131bcebc753933
Filipe Rangel <filiperangel@gmail.com>
2021-05-31 22:16:10 +0100

    commit files

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace unrailedSaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.restoreLastSaveBtn = new System.Windows.Forms.Button();
+            this.selectFolderBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // restoreLastSaveBtn
+            // 
+            this.restoreLastSaveBtn.Location = new System.Drawing.Point(126, 179);
+            this.restoreLastSaveBtn.Name = "restoreLastSaveBtn";
+            this.restoreLastSaveBtn.Size = new System.Drawing.Size(245, 94);
+            this.restoreLastSaveBtn.TabIndex = 0;
+            this.restoreLastSaveBtn.Text = "Restaurar Ultimo Save";
+            this.restoreLastSaveBtn.UseVisualStyleBackColor = true;
+            this.restoreLastSaveBtn.Click += new System.EventHandler(this.restoreLastSaveBtn_Click);
+            // 
+            // selectFolderBtn
+            // 
+            this.selectFolderBtn.Location = new System.Drawing.Point(126, 307);
+            this.selectFolderBtn.Name = "selectFolderBtn";
+            this.selectFolderBtn.Size = new System.Drawing.Size(245, 94);
+            this.selectFolderBtn.TabIndex = 1;
+            this.selectFolderBtn.Text = "Selecionar Pasta do Jogo";
+            this.selectFolderBtn.UseVisualStyleBackColor = true;
+            this.selectFolderBtn.Click += new System.EventHandler(this.selectFolderBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::unrailedSaver.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.selectFolderBtn);
+            this.Controls.Add(this.restoreLastSaveBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Unrailed Saver";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button restoreLastSaveBtn;
+        private System.Windows.Forms.Button selectFolderBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace unrailedSaver
+{
+    public partial class Form1 : Form
+    {
+        private string gameSavePath = "";
+        private bool isRestoring = false;
+        private static System.IO.FileSystemWatcher m_Watcher;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] lines = null;
+            try
+            {
+                lines = File.ReadAllLines(@".\config.txt");
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    StreamWriter sw = new StreamWriter(@".\config.txt", true, Encoding.ASCII);
+                    gameSavePath = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + @"\AppData\Local\Daedalic Entertainment GmbH\Unrailed\SaveGames\";
+                    sw.Write(gameSavePath);
+
+                    sw.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Executing finally block.");
+                }
+            }
+
+            if (lines != null)
+            {
+                gameSavePath = lines[0];
+            }
+
+            m_Watcher = new System.IO.FileSystemWatcher();
+            m_Watcher.Filter = "*.*";
+            m_Watcher.Path = gameSavePath;
+            m_Watcher.IncludeSubdirectories = false;
+            m_Watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            m_Watcher.Created += new FileSystemEventHandler(OnChanged);
+            m_Watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                if (!isRestoring)
+                {
+                    System.IO.Directory.CreateDirectory(gameSavePath + @"\bkp");
+                    System.IO.File.Copy(e.FullPath, gameSavePath + @"bkp\" + DateTime.Now.ToString("dd-MM-yyyy-H-mm-ss") + ".sav", true);
+                }
+                else
+                {
+                    isRestoring = false;
+                }
+            } catch (Exception err) {}
+        }
+
+        private void restoreLastSaveBtn_Click(object sender, EventArgs e)
+        {
+            isRestoring = true;
+            var directory = new DirectoryInfo(gameSavePath + @"bkp\");
+            try
+            {
+                var lastFile = directory.GetFiles().OrderByDescending(f => f.LastWriteTime).First();
+                File.Copy(lastFile.FullName, gameSavePath + "slot1.sav", true);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("No File to Restore.");
+            }
+        }
+
+        private void selectFolderBtn_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = gameSavePath;
+            DialogResult result = fbd.ShowDialog();
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                MessageBox.Show(fbd.SelectedPath);
+            }
+        }
+    }
+}
